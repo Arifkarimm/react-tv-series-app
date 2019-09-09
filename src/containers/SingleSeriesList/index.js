17b2d9cf6342d1c47312209dc59acddff4c05ca1
Fazlul <fkarif.cse@gmail.com>
2019-09-09 16:14:26 +0200

    singlepage list movie working

--- src/containers/SingleSeriesList/index.js
+++ src/containers/SingleSeriesList/index.js
@@ -1,10 +1,39 @@
 import React,{ Component } from "react";
 
 class SingleSeriesList extends Component {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+      singleMovieItem: null
+    }
+
+  }
+
+   componentDidMount(){ 
+  		const { id } = this.props.match.params;
+	    fetch(`https://api.tvmaze.com/shows/${id}?embed=episodes`)
+	    .then(response => response.json())
+	    .then(result => this.setState({ singleMovieItem: result }))
+  	}
+
 	render(){
-		console.log(this.props);
+		
+		const { singleMovieItem } = this.state;
 		return(
-			<p>single series list item {this.props.match.params.id}</p>
+			<div>
+				{singleMovieItem !== null && (
+					<div>
+						<p>{singleMovieItem.name}</p>
+						<p>Premiered - {singleMovieItem.premiered}</p>
+						<p>Rating - {singleMovieItem.rating.average}</p>
+						<p>Episodes - {singleMovieItem._embedded.episodes.length}</p>
+						<p>
+							<img src={singleMovieItem.image.medium} alt="singleMovieItem" />
+						</p>
+					</div>
+				)}
+			</div>
 		)
 	}
 
