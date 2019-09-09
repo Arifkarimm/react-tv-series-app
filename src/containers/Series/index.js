7056532ae9dc6578d3da25212f49f47cbaf593b2
Fazlul <fkarif.cse@gmail.com>
2019-09-09 13:44:51 +0200

    movie list validation work

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -8,29 +8,46 @@ class Series extends Component {
     this.state = {
       movieList: [],
       searchTerm: "",
+      isDataFetch: false
     }
 
     this.fetchSearcMovieStories = this.fetchSearcMovieStories.bind(this);
   }
 
 
-  	fetchSearcMovieStories(e){
-	    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+  	fetchSearcMovieStories(event){
+  		this.setState({ searchTerm: event.target.value, isDataFetch: true });
+	    fetch(`http://api.tvmaze.com/search/shows?q=${event.target.value}`)
 	    .then(response => response.json())
-	    .then(result => this.setState({ movieList: result}))
+	    .then(result => this.setState({ movieList: result, isDataFetch: false }))
   	}
 
 
 	render(){
+		const { movieList, searchTerm, isDataFetch } = this.state;
 		return(
 			<div>
+				<div>
+					<input 
+						type="text" 
+						value={searchTerm} 
+						onChange={this.fetchSearcMovieStories} 
+					/>
+				</div>
+
+
+			{!isDataFetch && movieList.length === 0 && searchTerm.trim() === "" && (
+				<p>Please enter the movie list keyword</p>
+			)}
+
+			{!isDataFetch && movieList.length === 0 && searchTerm.trim() !== "" && (
+				<p>there is no such kind of movie in this list</p>
+			)}
+
+			{!isDataFetch && <SeriesList list={movieList} /> }
+
 			
-
-			<input type="text" onChange={this.fetchSearcMovieStories} />
-
-			<SeriesList list={this.state.movieList} />
 			</div>
-			
 		)
 	}
 }
